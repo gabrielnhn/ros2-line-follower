c9e72ac94e222b254cfa12ae03dd7220cc3c41ab
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-23 20:01:01 -0300

    update description

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     zip_safe=True,
     maintainer='Gabriel Nascarella Hishida',
     maintainer_email='gabrielnhn@ufpr.br',
-    description='Control a turtlebot to follow a RoboCore style track',
+    description='Have a turtlebot follow a RoboCore style track ',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
