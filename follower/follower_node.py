3dd991afd5159dfe019144369e985f70ca7337de
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-26 10:32:26 -0300

    feat: Implement ROS2 Timer system (synchronous system)

--- follower/follower_node.py
+++ follower/follower_node.py
@@ -26,7 +26,7 @@ def image_callback(msg):
     """Function to be called whenever a new Image message arrives"""
     global image_input
     image_input = bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-    node.get_logger().info('Received image')
+    # node.get_logger().info('Received image')
 
 def get_contour_centroid(mask, out):
     """
@@ -60,95 +60,100 @@ def get_contour_centroid(mask, out):
 
     return centroid
 
-def main():
+def timer_callback():
     global error
     global image_input
     global just_seen
 
     # Wait for the first image to be received
     while type(image_input) != np.ndarray:
-        rclpy.spin_once(node)
-    
+        # rclpy.spin_once(node)
+        pass
+
     height, width, _ = image_input.shape
 
-    while rclpy.ok():
+    # while rclpy.ok():
 
-        image = image_input.copy()
+    image = image_input.copy()
 
-        global crop_w_start
-        crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = crop_size(height, width)
+    global crop_w_start
+    crop_h_start, crop_h_stop, crop_w_start, crop_w_stop = crop_size(height, width)
 
-        # get the bottom part of the image (matrix slicing)
-        crop = image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
-        
-        # get a binary picture, where non-zero values represent the line.
-        mask = cv2.inRange(crop, lower_bgr_values, upper_bgr_values)
+    # get the bottom part of the image (matrix slicing)
+    crop = image[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop]
+    
+    # get a binary picture, where non-zero values represent the line.
+    mask = cv2.inRange(crop, lower_bgr_values, upper_bgr_values)
 
-        cv2.imshow('mask', mask)
+    cv2.imshow('mask', mask)
 
-        # get the centroid of the biggest contour in the picture,
-        # and plot its detail on the bottom part of the output image
-        output = image
-        centroid = get_contour_centroid(mask, output[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop])  
+    # get the centroid of the biggest contour in the picture,
+    # and plot its detail on the bottom part of the output image
+    output = image
+    centroid = get_contour_centroid(mask, output[crop_h_start:crop_h_stop, crop_w_start:crop_w_stop])  
 
-        # error: The difference between the center of the camera
-        # and the center of the line
+    # error: The difference between the center of the camera
+    # and the center of the line
 
-        message = Twist()
-        
-        if centroid:
-            # if there even is a centroid:
-            # (as the camera could not be reading any lines)      
-            x = centroid['x']
-            error = x - width//2
-            print("Error:{}".format(error))
-            message.linear.x = LINEAR
-            just_seen = True
-        else:
-            if just_seen:
-                just_seen = False
-                error = error * 1.2
-            message.linear.x = 0.0
+    message = Twist()
+    
+    if centroid:
+        # if there even is a centroid:
+        # (as the camera could not be reading any lines)      
+        x = centroid['x']
+        error = x - width//2
+        print("Error:{}".format(error))
+        message.linear.x = LINEAR
+        just_seen = True
+    else:
+        if just_seen:
+            just_seen = False
+            error = error * 1.2
+        message.linear.x = 0.0
 
 
-        # Create an empty Twist message, then give it values
-        # THESE ARE THE CONTROL VALUES TO BE CHANGED AND TESTED
-        message.angular.z = float(error) * -KP
-        print("Linear Z: {}".format(message.linear.z))
+    # Create an empty Twist message, then give it values
+    # THESE ARE THE CONTROL VALUES TO BE CHANGED AND TESTED
+    message.angular.z = float(error) * -KP
+    print("Angular Z: {}".format(message.angular.z))
 
-        # Plot the boundaries where the image was cropped
-        cv2.rectangle(output, (crop_w_start, crop_h_start), (crop_w_stop, crop_h_stop), (0,0,255), 2)
+    # Plot the boundaries where the image was cropped
+    cv2.rectangle(output, (crop_w_start, crop_h_start), (crop_w_stop, crop_h_stop), (0,0,255), 2)
 
-        if centroid:
-            # If there is a line to be followed, print its centroid
-            cv2.circle(output, (centroid['x'], crop_h_start + centroid['y']), 5, (0,255,0), 7)
+    if centroid:
+        # If there is a line to be followed, print its centroid
+        cv2.circle(output, (centroid['x'], crop_h_start + centroid['y']), 5, (0,255,0), 7)
 
-        # Uncomment to print the binary picture
-        #cv2.imshow("mask", mask)
+    # Uncomment to print the binary picture
+    #cv2.imshow("mask", mask)
 
-        # Show the output image to the user
-        cv2.imshow("output", output)
-        # Print the image for 5milis, then resume the execution
-        cv2.waitKey(5)
+    # Show the output image to the user
+    cv2.imshow("output", output)
+    # Print the image for 5milis, then resume the execution
+    cv2.waitKey(5)
 
-        # Publish the message to 'cmd_vel'
-        publisher.publish(message)
-        time.sleep(timer_period)
-        rclpy.spin_once(node)
+    # Publish the message to 'cmd_vel'
+    publisher.publish(message)
+    # time.sleep(timer_period)
+    # rclpy.spin_once(node)
 
 
 # BGR values to filter only the color of the line
 lower_bgr_values = np.array([31,  42,  53])
 upper_bgr_values = np.array([238, 142, 158])
 
-rclpy.init()
-node = Node('follower')
-publisher = node.create_publisher(Twist, '/cmd_vel', 3)
-
-subscription = node.create_subscription(Image, 'camera/image_raw',
-                                        image_callback,
-                                        rclpy.qos.qos_profile_sensor_data)
-timer_period = 0.06 # seconds
+def main():
+    rclpy.init()
+    global node
+    node = Node('follower')
+    global publisher
+    publisher = node.create_publisher(Twist, '/cmd_vel', rclpy.qos.qos_profile_system_default)
+    subscription = node.create_subscription(Image, 'camera/image_raw',
+                                            image_callback,
+                                            rclpy.qos.qos_profile_sensor_data)
+    timer_period = 0.06 # seconds
+    timer = node.create_timer(timer_period, timer_callback)
+    rclpy.spin(node)
 
 try:
     main()
