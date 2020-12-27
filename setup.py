a63704f8a078cec706cbca76a02e6edc63612546
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-27 00:08:20 -0300

    feat: Add worlds/* and launch/* to shared folder

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('worlds/*.world'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
