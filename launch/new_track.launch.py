2a70681b21ba2900b1003e0f5249c126822cf150
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-26 23:50:28 -0300

    add: Trying to implement new world

---
+++ launch/new_track.launch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Adapted from:
+# Copyright 2019 ROBOTIS CO., LTD.
+# Authors: Darby Lim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_file_name = 'new_track2.world'
+    world = os.path.join(get_package_share_directory('follower'),
+                         'worlds', world_file_name)
+    launch_file_dir = os.path.join(get_package_share_directory('follower'), 'launch')
+    turtlebot_launch_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            ),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([turtlebot_launch_dir, '/robot_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+    ])
