042348eafecb656a9d60d8de834d174ef1451df3
Gabriel Nascarella <ielnascarella@hotmail.com>
2020-12-28 16:50:59 -0300

    parameter: update MIN_AREA

--- follower/follower_node.py
+++ follower/follower_node.py
@@ -14,7 +14,7 @@ bridge = cv_bridge.CvBridge()
 
 ## User-defined macros: (Update these values to your liking)
 # Minimum size for a contour to be considered
-MIN_AREA = 10000 
+MIN_AREA = 8000 
 
 # Robot's speed when following the line
 LINEAR_SPEED = 0.2
